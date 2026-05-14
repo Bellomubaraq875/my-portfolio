1bd93e4363975254bb4507305cd3feaf8309eb5a
Bellomubaraq875 <bellomubaraq875@gmail.com>
2026-05-14 14:30:30 +0100

    magnetic button added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 
 // import Header from "./layout/Header";
+import HeroSection from "./section/Hero";
+
 
 export default function Home() {
   return (
-    // <Header/>
-    <div className=""></div>
+    
+    <HeroSection/> 
   );
 }
